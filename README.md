b32fc36a1bcc5a3d5eab6c4a576c640478bd3c7e
GLXB <ustynow.gleb@gmail.com>
2024-09-18 02:19:27 +0300

    feat: The very base version, just enough to get started.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # aio_app
 Asyncio app with graceful shutdown and pre-/post-shutdown hooks.
+
+## Installation
+Submodule it or whatever, then import.
+
+## Usage
+```python
+from asyncio import run
+from json import dumps, loads
+
+from aio_app import App
+from websockets import connect
+
+
+class Bpp:
+    def __init__(self):
+        super().__init__()
+        # stuff
+
+    # This one will only start if shutdown has not been initiated
+    @App.worker
+    async def foo(self, bar):
+        # buzzing around
+        pass
+
+    # You have to redefine this, the app will use this method as an entry point
+    async def main(self):
+        uri = 'wss://some.portal/api/data'
+        ws = await connect(uri)
+        await ws.send(dumps(dict(method='some_api_method')))
+        async for message in ws:
+            message = loads(message)
+            await foo(message)
+            if self.shutdown:
+                kms() # right now you need to watch for shutdown for yourself,
+                      # I might change this later in favour of exception
+                      # on faulty worker start
+        await ws.close()
+        
+    # These will be called when shutdown is initiated
+    # Ordering is a thing
+    @App.shutdown_hook
+    def say_goodbye(self):
+        print('See you, space cowboy...')
+        
+    @App.shutdown_hook
+    def pray(self):
+        print('Amen')
+    
+    # And this will be called when all the workers have stopped
+    @App.post_shutdown_hook
+    def bury(self):
+        print('Shovels go brrrrrrr')
+```
---
+++ aio_app.py
@@ -0,0 +1,55 @@
+from asyncio import all_tasks, create_task, current_task, get_running_loop
+from signal import SIGHUP, SIGINT, SIGTERM
+
+
+class App:
+    shutdown_hooks = []
+    post_shutdown_hooks = []
+
+    def __init__(self):
+        self.shutdown = False
+
+    async def start(self):
+        """Start app with this one."""
+        loop = get_running_loop()
+        for s in (SIGHUP, SIGINT, SIGTERM):
+            loop.add_signal_handler(
+                s,
+                lambda sig=s: create_task(self.graceful_shutdown(sig))
+            )
+        return await self.main()
+
+    @abstractmethod
+    async def main(self):
+        """Your application entry point."""
+        pass
+
+    async def graceful_shutdown(self, signal):
+        self.shutdown = True
+        for hook in App.shutdown_hooks:
+            hook(self)
+        tasks = [t for t in all_tasks()
+                 if t is not current_task()]
+
+        await gather(*tasks, return_exceptions=True)
+        get_running_loop().stop()
+        for hook in App.post_shutdown_hooks:
+            hook()
+
+    @staticmethod
+    def worker(method):
+        """Entry point to an async worker. Wouldn't start if shutdown has been initiated."""
+        @wraps(method)
+        async def wrapper(*args, **kwargs):
+            if not args[0].shutdown:
+                return await method(*args, **kwargs)
+
+        return wrapper
+
+    @classmethod
+    def shutdown_hook(cls, hook):
+        cls.shutdown_hooks.append(hook)
+
+    @classmethod
+    def post_shutdown_hook(cls, hook):
+        cls.post_shutdown_hooks.append(hook)
